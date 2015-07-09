d6843da95af28ae8d5eba0c75ff1eed14b138065
Matheus Bernardes <mthbernardes@gmail.com>
2015-07-09 19:39:07 -0300

    Create tor_installer.sh

---
+++ tor_installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo '[+] - Tor installation'
+echo '[+] - Developed by The_Gambler'
+echo '[+] - https://github.com/mthbernardes/'
+echo '[+] - https://www.facebook.com/mthbernardes'
+echo
+
+echo '[+] - Adding Repo...'
+echo deb http://deb.torproject.org/torproject.org wheezy main > /etc/apt/sources.list.d/tor.list
+
+echo '[+] - Adding keys...'
+echo 
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add –
+echo 
+
+echo '[+] - Updating...'
+apt-get update > /dev/null
+echo 
+
+echo '[+] - Installing... '
+echo 
+apt-get install deb.torproject.org-keyring -y > /dev/null
+apt-get install tor -y > /dev/null
+apt-get install privoxy -y > /dev/null
+echo 
+
+echo '[+] - Changing configuration files'
+sed -i '776s/.*/listen-address  127.0.0.1:8118/' /etc/privoxy/config
+sed -i '1292s/#/ /' /etc/privoxy/config
+echo 
+
+echo '[+] - Rebooting service and adding to boot'
+echo 
+/etc/init.d/tor restart && /etc/init.d/privoxy restart  && update-rc.d tor enable && update-rc.d privoxy enable
+echo 
+
+echo '[+] - Finished'
+echo '[+] - Enjoy your TOR Connection'
